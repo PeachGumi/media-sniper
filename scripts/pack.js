76b6dd66d8a028d1425944b7e2e0c67a0f208837
PeachGumi <49714852+PeachGumi@users.noreply.github.com>
2026-08-25 07:03:30 +0900

    build: package popup metadata script

--- scripts/pack.js
+++ scripts/pack.js
@@ -22,6 +22,7 @@ const includes = [
   'src/libav/libav-6.5.7.1-h264-aac-mp3.wasm.wasm',
   'popup/popup.html',
   'popup/popup.js',
+  'popup/meta.js',
   'popup/options.html',
   'popup/options.js',
   'icons/icon16.png',
